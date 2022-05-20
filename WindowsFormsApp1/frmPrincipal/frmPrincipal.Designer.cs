a23b935074025dad628a09a416a396557b9d39cc
OrlandO230702 <00227722@uca.edu.sv>
2022-05-20 16:06:00 -0600

    GUI

---
+++ WindowsFormsApp1/frmPrincipal/frmPrincipal.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnl3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblRebajas = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.btnCompra2 = new System.Windows.Forms.Button();
+            this.btnCompra3 = new System.Windows.Forms.Button();
+            this.btnCompra4 = new System.Windows.Forms.Button();
+            this.btnCompra1 = new System.Windows.Forms.Button();
+            this.pnl1.SuspendLayout();
+            this.pnl2.SuspendLayout();
+            this.pnl3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pnl1.Controls.Add(this.label2);
+            this.pnl1.Controls.Add(this.label1);
+            this.pnl1.Location = new System.Drawing.Point(-5, -2);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(896, 46);
+            this.pnl1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(613, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contáctanos al 800-22000-722";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿Como comprar?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl2
+            // 
+            this.pnl2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.pnl2.Controls.Add(this.label6);
+            this.pnl2.Controls.Add(this.textBox1);
+            this.pnl2.Controls.Add(this.label3);
+            this.pnl2.Controls.Add(this.label4);
+            this.pnl2.Location = new System.Drawing.Point(-2, 35);
+            this.pnl2.Name = "pnl2";
+            this.pnl2.Size = new System.Drawing.Size(899, 85);
+            this.pnl2.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(214, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "¿Qué estás buscando?\r\n";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(495, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(371, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mis listas |   Mi cuenta  |   Mis pedidos |   Mi carrito\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(32, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Walmart";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl3
+            // 
+            this.pnl3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pnl3.Controls.Add(this.label5);
+            this.pnl3.Location = new System.Drawing.Point(1, 159);
+            this.pnl3.Name = "pnl3";
+            this.pnl3.Size = new System.Drawing.Size(896, 41);
+            this.pnl3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(188, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(471, 41);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "¡Bienvenido a tu Walmart! Disfruta tu experiencia de compra en línea";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(54, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 23);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Selecciona tu tienda";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblRebajas
+            // 
+            this.lblRebajas.ForeColor = System.Drawing.Color.Crimson;
+            this.lblRebajas.Location = new System.Drawing.Point(423, 123);
+            this.lblRebajas.Name = "lblRebajas";
+            this.lblRebajas.Size = new System.Drawing.Size(136, 35);
+            this.lblRebajas.TabIndex = 4;
+            this.lblRebajas.Text = "REBAJAS";
+            this.lblRebajas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(565, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 23);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Nuestras marcas";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(694, 133);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 23);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Tamaños familiares";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(54, 222);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(54, 425);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(368, 142);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(448, 425);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(382, 142);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(54, 587);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(368, 103);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(448, 587);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(382, 103);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // btnCompra2
+            // 
+            this.btnCompra2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompra2.Location = new System.Drawing.Point(545, 509);
+            this.btnCompra2.Name = "btnCompra2";
+            this.btnCompra2.Size = new System.Drawing.Size(90, 26);
+            this.btnCompra2.TabIndex = 13;
+            this.btnCompra2.Text = "Compra aquí";
+            this.btnCompra2.UseVisualStyleBackColor = true;
+            this.btnCompra2.Click += new System.EventHandler(this.btnCompra2_Click);
+            // 
+            // btnCompra3
+            // 
+            this.btnCompra3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompra3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnCompra3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnCompra3.Location = new System.Drawing.Point(348, 647);
+            this.btnCompra3.Name = "btnCompra3";
+            this.btnCompra3.Size = new System.Drawing.Size(74, 43);
+            this.btnCompra3.TabIndex = 14;
+            this.btnCompra3.Text = "Compra aquí";
+            this.btnCompra3.UseVisualStyleBackColor = true;
+            this.btnCompra3.Click += new System.EventHandler(this.btnCompra3_Click);
+            // 
+            // btnCompra4
+            // 
+            this.btnCompra4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompra4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnCompra4.Location = new System.Drawing.Point(736, 647);
+            this.btnCompra4.Name = "btnCompra4";
+            this.btnCompra4.Size = new System.Drawing.Size(94, 32);
+            this.btnCompra4.TabIndex = 15;
+            this.btnCompra4.Text = "Obtener rebajas";
+            this.btnCompra4.UseVisualStyleBackColor = true;
+            this.btnCompra4.Click += new System.EventHandler(this.btnCompra4_Click);
+            // 
+            // btnCompra1
+            // 
+            this.btnCompra1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompra1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCompra1.Location = new System.Drawing.Point(332, 509);
+            this.btnCompra1.Name = "btnCompra1";
+            this.btnCompra1.Size = new System.Drawing.Size(90, 26);
+            this.btnCompra1.TabIndex = 16;
+            this.btnCompra1.Text = "Compra aquí";
+            this.btnCompra1.UseVisualStyleBackColor = true;
+            this.btnCompra1.Click += new System.EventHandler(this.btnCompra1_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 787);
+            this.Controls.Add(this.btnCompra1);
+            this.Controls.Add(this.btnCompra4);
+            this.Controls.Add(this.btnCompra3);
+            this.Controls.Add(this.btnCompra2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblRebajas);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pnl3);
+            this.Controls.Add(this.pnl2);
+            this.Controls.Add(this.pnl1);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Walmart";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.pnl1.ResumeLayout(false);
+            this.pnl2.ResumeLayout(false);
+            this.pnl2.PerformLayout();
+            this.pnl3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btnCompra1;
+
+
+        private System.Windows.Forms.Button btnCompra2;
+        private System.Windows.Forms.Button btnCompra3;
+        private System.Windows.Forms.Button btnCompra4;
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+
+        private System.Windows.Forms.Label lblRebajas;
+
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel pnl3;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel pnl2;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Panel pnl1;
+
+        #endregion
+    }
+
+    internal class frmPrincipal
+    {
+    }
+}
---
+++ WindowsFormsApp1/frmPrincipal/frmPrincipal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            pnl1.BackColor = Color.FromArgb(0,92,181);
+            pnl2.BackColor = Color.FromArgb(26,117,207);
+            lblRebajas.BackColor = Color.FromArgb(252,237,52);
+            pnl3.BackColor = Color.FromArgb(0,92,181);
+            btnCompra1.BackColor = Color.FromArgb(25,128,215);
+            btnCompra2.BackColor = Color.FromArgb(25,128,215);
+            btnCompra3.BackColor = Color.FromArgb(255, 255, 255);
+            btnCompra4.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void btnCompra1_Click(object sender, EventArgs e)
+        {
+            string mensaje = "Usted ha comprado una secadora.";
+            MessageBox.Show(mensaje, "Walmart", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void btnCompra2_Click(object sender, EventArgs e)
+        {
+            string mensaje = "Usted ha comprado raid para zancudos.";
+            MessageBox.Show(mensaje, "Walmart", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCompra3_Click(object sender, EventArgs e)
+        {
+            string mensaje = "Usted ha comprado un telefono con 10 meses sin intereses.";
+            MessageBox.Show(mensaje, "Walmart", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void btnCompra4_Click(object sender, EventArgs e)
+        {
+            string mensaje = "Usted ha obtenido rebajas.";
+            MessageBox.Show(mensaje, "Walmart", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
